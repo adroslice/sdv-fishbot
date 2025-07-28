b0bea59f422d3ac17899e4d1062811ad925c3db3
adro <a.schlett@gmx.de>
2025-07-28 22:29:18 +0200

    initial commit

---
+++ ModConfig.cs
@@ -0,0 +1,171 @@
+namespace Fishbot;
+using StardewModdingAPI;
+
+public sealed class ModConfig
+{
+    // Keybinds
+    public SButton ToggleAutomationKey { get; set; } = SButton.F5;
+
+    // Automations
+    public bool DoAutoCast { get; set; } = true;
+    public bool DoAutoHit { get; set; } = true;
+    public bool DoAutoPlay { get; set; } = true;
+    public bool DoAutoStow { get; set; } = true;
+    public bool DoAutoLoot { get; set; } = true;
+
+    // Utility
+    public bool EnableBubbleRadar { get; set; } = true;
+
+    // FishingAutomaton+ Strategy Parameters
+    public int FA_PredictLinearFrames { get; set; } = 5;
+    public float FA_RelativeOffsetBarPercentage { get; set; } = 0.7f;
+    public float FA_PrioTreasureAbove { get; set; } = 0.70f;
+    public float FA_PrioFishBelow { get; set; } = 0.35f;
+    public float FA_MinBarTargetVelocity { get; set; } = 1.0F;
+    public float FA_MaxBarVelocityNearBottom { get; set; } = 2.0F;
+    public float FA_MaxBarVelocity { get; set; } = 6.0F;
+    public float FA_BottomThreshold { get; set; } = 30f;
+
+    public void SetupConfigOptions(IGenericModConfigMenuApi configMenu, IManifest mod)
+    {
+        configMenu.AddSectionTitle(mod: mod, text: () => "Keybinds");
+        configMenu.AddKeybind(
+            mod: mod,
+            name: () => "Toggle Automation",
+            tooltip: () => "Pressing this key toggles the fishing automation on and off.",
+            getValue: () => this.ToggleAutomationKey,
+            setValue: value => this.ToggleAutomationKey = value
+        );
+
+        configMenu.AddSectionTitle(mod: mod, text: () => "Automations", tooltip: () => "Here you can configure what you would like Fishbot to automate for you when enabled.");
+        configMenu.AddBoolOption(
+            mod: mod,
+            name: () => "Auto-Cast",
+            getValue: () => this.DoAutoCast,
+            setValue: value => { this.DoAutoCast = value; }
+        );
+        configMenu.AddBoolOption(
+            mod: mod,
+            name: () => "Auto-Hit",
+            getValue: () => this.DoAutoHit,
+            setValue: value => { this.DoAutoHit = value; }
+        );
+        configMenu.AddBoolOption(
+            mod: mod,
+            name: () => "Auto-Play Minigame",
+            getValue: () => this.DoAutoPlay,
+            setValue: value => { this.DoAutoPlay = value; }
+        );
+        configMenu.AddBoolOption(
+            mod: mod,
+            name: () => "Auto-Stow Fish",
+            getValue: () => this.DoAutoStow,
+            setValue: value => { this.DoAutoStow = value; }
+        );
+        configMenu.AddBoolOption(
+            mod: mod,
+            name: () => "Auto-Loot Treasure",
+            getValue: () => this.DoAutoLoot,
+            setValue: value => { this.DoAutoLoot = value; }
+        );
+
+        configMenu.AddSectionTitle(mod: mod, text: () => "Utility", tooltip: () => "Little features to improve your fishing experience.");
+        configMenu.AddBoolOption(
+            mod: mod,
+            getValue: () => this.EnableBubbleRadar,
+            setValue: value => this.EnableBubbleRadar = value,
+            name: () => "Bubble Radar",
+            tooltip: () => "Indicates the direction of active bubbles on with an arrow near your character."
+        );
+
+        configMenu.AddSectionTitle(mod: mod, text: () => "Minigame Strategies", tooltip: () => "Here you can decide on which strategy to use and fine-tune its values.");
+        configMenu.AddPageLink(
+            mod: mod,
+            pageId: nameof(ModEntry.MinigameStrategyFishingAutomatonPlus),
+            text: () => "FishingAutomatonPlus Strategy",
+            tooltip: () => "You can fine tune the variables of this fishing strategy here."
+        );
+
+        configMenu.AddPage(mod: mod, pageId: nameof(ModEntry.MinigameStrategyFishingAutomatonPlus), pageTitle: () => "\"FishingAutomaton+\" Strategy");
+        configMenu.AddNumberOption(
+            mod: mod,
+            getValue: () => this.FA_PredictLinearFrames,
+            setValue: value => this.FA_PredictLinearFrames = value,
+            name: () => nameof(this.FA_PredictLinearFrames),
+            tooltip: () => "Predict fish position in X frames of continued linear movement. Use 0 to disable. (inaccurate, but helps track fast targets)",
+            min: 0,
+            max: 20,
+            interval: 1
+        );
+        configMenu.AddNumberOption(
+            mod: mod,
+            getValue: () => this.FA_RelativeOffsetBarPercentage,
+            setValue: value => this.FA_RelativeOffsetBarPercentage = value,
+            name: () => nameof(this.FA_RelativeOffsetBarPercentage),
+            tooltip: () => "Fishing bar offset towards the track center relative to size (multiplied by this value) and position. Use 0 for always keeping it centered. (Fish are more likely to move towards the center)",
+            min: 0.0f,
+            max: 1f,
+            interval: 0.01f
+        );
+        configMenu.AddNumberOption(
+            mod: mod,
+            getValue: () => this.FA_PrioTreasureAbove,
+            setValue: value => this.FA_PrioTreasureAbove = value,
+            name: () => nameof(this.FA_PrioTreasureAbove),
+            tooltip: () => "Prioritize treasure if the capture progress exceeds this threshold. Use 1.0 to ignore treasure.",
+            min: 0.0f,
+            max: 1.0f,
+            interval: 0.01f
+        );
+        configMenu.AddNumberOption(
+            mod: mod,
+            getValue: () => this.FA_PrioFishBelow,
+            setValue: value => this.FA_PrioFishBelow = value,
+            name: () => nameof(this.FA_PrioFishBelow),
+            tooltip: () => "Prioritize fish if the capture progress falls below this threshold. This value needs to be smaller than FA_PrioTreasureAbove.",
+            min: 0.0f,
+            max: 1.0f,
+            interval: 0.01f
+        );
+        configMenu.AddNumberOption(
+            mod: mod,
+            getValue: () => this.FA_MinBarTargetVelocity,
+            setValue: value => this.FA_MinBarTargetVelocity = value,
+            name: () => nameof(this.FA_MinBarTargetVelocity),
+            tooltip: () => "Minimum target velocity when tracking fish.",
+            min: 0.0f,
+            max: 5.0f,
+            interval: 0.01f
+        );
+        configMenu.AddNumberOption(
+            mod: mod,
+            getValue: () => this.FA_MaxBarVelocityNearBottom,
+            setValue: value => this.FA_MaxBarVelocityNearBottom = value,
+            name: () => nameof(this.FA_MaxBarVelocityNearBottom),
+            tooltip: () => "Maximum velocity near the bottom edge, lower values mitigate bouncing.",
+            min: 0.0f,
+            max: 5.0f,
+            interval: 0.01f
+        );
+        configMenu.AddNumberOption(
+            mod: mod,
+            getValue: () => this.FA_MaxBarVelocity,
+            setValue: value => this.FA_MaxBarVelocity = value,
+            name: () => nameof(this.FA_MaxBarVelocity),
+            tooltip: () => "Maximum fishing bar speed, lower values mitigate overshoot.",
+            min: 0.0f,
+            max: 10.0f,
+            interval: 0.01f
+        );
+        configMenu.AddNumberOption(
+            mod: mod,
+            getValue: () => this.FA_BottomThreshold,
+            setValue: value => this.FA_BottomThreshold = value,
+            name: () => nameof(this.FA_BottomThreshold),
+            tooltip: () => "How far from the edge FA_MaxBarVelocityNearBottom starts to take effect, higher values mitigate bouncing.",
+            min: 0.0f,
+            max: 100.0f,
+            interval: 1f
+        );
+    }
+}
---
+++ ModEntry.cs
@@ -0,0 +1,213 @@
+namespace Fishbot;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Tools;
+using StardewValley.Menus;
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+internal sealed class ModEntry : Mod
+{
+    private ModConfig Config = new ModConfig();
+    private bool AutomationEnabled = false;
+    private MethodInfo? SmapiInternalOverrideButtonMethod;
+    private void ClickIf(bool cond) => SmapiInternalOverrideButtonMethod?.Invoke(Game1.input, new object[] { SButton.C, cond });
+
+    public override void Entry(IModHelper helper)
+    {
+        SmapiInternalOverrideButtonMethod = Game1.input.GetType().GetMethod("OverrideButton")
+           ?? throw new InvalidOperationException("Can't find 'OverrideButton' method on SMAPI's input class. This means the mod needs to be updated to use a new input simulation method.");
+
+        helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+        helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        helper.Events.GameLoop.UpdateTicked += this.OnUpdate;
+        helper.Events.Display.RenderingHud += OnRenderingHud;
+    }
+
+    private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
+    {
+        var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+        if (configMenu is null)
+            return;
+
+        configMenu.Register(
+            mod: this.ModManifest,
+            reset: () => this.Config = new ModConfig(),
+            save: () => this.Helper.WriteConfig(this.Config)
+        );
+
+        Config.SetupConfigOptions(configMenu, this.ModManifest);
+    }
+
+    // Toggles the Automations
+    private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+    {
+        if (e.Button == Config.ToggleAutomationKey) AutomationEnabled = !AutomationEnabled;
+    }
+
+    // Renders the Activity Indicator and Bubble Radar
+    private void OnRenderingHud(object? sender, StardewModdingAPI.Events.RenderingHudEventArgs e)
+    {
+        if (!Context.IsWorldReady) return;
+        if (Config.EnableBubbleRadar && !(Game1.currentLocation.fishSplashPoint.X == 0 && Game1.currentLocation.fishSplashPoint.Y == 0))
+        {
+            var screenCenter = new Vector2(
+                Game1.viewport.Width / 2,
+                Game1.viewport.Height / 2
+            );
+            var arrowDirection = Vector2.Normalize(new Vector2(
+                Game1.currentLocation.fishSplashPoint.X * 64f - (float)Game1.viewport.X + 32f,
+                Game1.currentLocation.fishSplashPoint.Y * 64f - (float)Game1.viewport.Y + 32f
+            ) - screenCenter);
+
+            e.SpriteBatch.Draw(
+                texture: Game1.mouseCursors,
+                position: screenCenter + arrowDirection * 64f * 2f,
+                sourceRectangle: new Microsoft.Xna.Framework.Rectangle(412, 495, 5, 4),
+                Color.White,
+                rotation: (float)(-Math.Atan2(arrowDirection.X, arrowDirection.Y) - Math.PI),
+                origin: new Vector2(2f, 2f),
+                scale: 4f,
+                SpriteEffects.None,
+                layerDepth: 1f
+            );
+        }
+
+        if (!AutomationEnabled) return;
+
+        int vpadding = 10, hpadding = 12;
+        var text = "Fishbot active!";
+        var textSize = Game1.smallFont.MeasureString(text);
+        var position = new Vector2(0, Game1.graphics.GraphicsDevice.Viewport.Height - textSize.Y - vpadding * 2);
+        IClickableMenu.drawTextureBox(e.SpriteBatch,
+            (int)position.X, (int)position.Y,
+            (int)textSize.X + hpadding * 2, (int)textSize.Y + vpadding * 2,
+            Color.White
+        );
+        e.SpriteBatch.DrawString(Game1.smallFont, text,
+            new Vector2(position.X + hpadding, position.Y + vpadding + 2),
+            Color.Black
+        );
+    }
+
+    // Runs the Automations
+    public void OnUpdate(object? sender, UpdateTickedEventArgs e)
+    {
+        if (!Context.IsWorldReady || !AutomationEnabled || Game1.player.CurrentTool is not FishingRod rod)
+            return;
+
+        Action automation = GetFishingState(rod) switch
+        {
+            FishingState.ReadyToCast when Config.DoAutoCast => () => ClickIf(true),
+            FishingState.TimingCast when Config.DoAutoCast => () => ClickIf(!(rod.castingPower >= 0.99f)),
+            FishingState.Nibbling when Config.DoAutoHit => () => rod.endUsing(Game1.currentLocation, Game1.player),
+            FishingState.Playing when Config.DoAutoPlay => () => ClickIf(MinigameStrategyFishingAutomatonPlus((BobberBar)Game1.activeClickableMenu)),
+            FishingState.ShowingTreasure when Config.DoAutoLoot => () => AcquireTreasure((ItemGrabMenu)Game1.activeClickableMenu),
+            FishingState.FishCaught when Config.DoAutoStow => () => ClickIf(true),
+            _ => () => { }
+        };
+        automation();
+    }
+
+    private enum FishingState { Idle, ReadyToCast, TimingCast, Casting, Waiting, Nibbling, Playing, ShowingTreasure, FishCaught }
+    private FishingState GetFishingState(FishingRod rod) => true switch
+    {
+        _ when Game1.activeClickableMenu is BobberBar => FishingState.Playing,
+        _ when Game1.activeClickableMenu is ItemGrabMenu => FishingState.ShowingTreasure,
+        _ when rod.fishCaught => FishingState.FishCaught,
+        _ when rod.isNibbling && !rod.isReeling && !rod.showingTreasure && !rod.treasureCaught => FishingState.Nibbling,
+        _ when rod.isFishing && !rod.isNibbling && !rod.hit => FishingState.Waiting,
+        _ when rod.isTimingCast => FishingState.TimingCast,
+        _ when rod.isCasting || rod.castedButBobberStillInAir => FishingState.Casting,
+        _ when Context.CanPlayerMove && Game1.activeClickableMenu == null => FishingState.ReadyToCast,
+        _ => FishingState.Idle
+    };
+
+    // Grabs all treasure from a GrabMenu and exits only if it could get out everything.
+    private void AcquireTreasure(ItemGrabMenu menu)
+    {
+        var actualInventory = menu.ItemsToGrabMenu.actualInventory;
+
+        for (int i = actualInventory.Count - 1; i >= 0; i--)
+            if (Game1.player.addItemToInventoryBool(actualInventory[i]))
+            {
+                Game1.addHUDMessage(new HUDMessage(actualInventory[i].DisplayName) { messageSubject = actualInventory[i] });
+                actualInventory.RemoveAt(i);
+            }
+
+        if (actualInventory.Count == 0) menu.exitThisMenu();
+    }
+
+    // FishingAutomaton+ Strategy
+    internal bool FA_PrioTreasure = false;
+    internal bool MinigameStrategyFishingAutomatonPlus(BobberBar bar)
+    {
+        // Derived values
+        float barCenter = bar.bobberBarPos + (bar.bobberBarHeight / 2) - 30f; // 30f here is a correction offset stemming from the bobbers height
+        float barMax = BobberBar.bobberBarTrackHeight - bar.bobberBarHeight;
+
+        // Target selection
+        if (bar.treasure && bar.treasurePosition > 0 && !bar.treasureCaught)
+        {
+            if (bar.distanceFromCatching > Config.FA_PrioTreasureAbove) FA_PrioTreasure = true;
+            else if (bar.distanceFromCatching < Config.FA_PrioFishBelow) FA_PrioTreasure = false;
+        }
+        else FA_PrioTreasure = false;
+        var targetPos = FA_PrioTreasure
+            ? (Math.Abs(bar.treasurePosition - bar.bobberPosition) < bar.bobberBarHeight * 0.8f
+                ? (bar.treasurePosition + bar.bobberPosition) / 2 // Target both if they fit within a 10% margin of the fishing bar
+                : bar.treasurePosition)                           // Otherwise, just go for the treasure
+            : Math.Clamp( // When targeting the fish, try to predict its future position without overshooting its own target...
+                bar.bobberPosition + bar.bobberSpeed * Config.FA_PredictLinearFrames,
+                Math.Min(bar.bobberTargetPosition, bar.bobberPosition),
+                Math.Max(bar.bobberTargetPosition, bar.bobberPosition) // ... and add an offset relative to the bars position and size to bias the bar towards the center and improve reactions to the most common moves
+            ) + bar.bobberBarHeight * Config.FA_RelativeOffsetBarPercentage / 2 - ((bar.bobberPosition / BobberBar.bobberTrackHeight) * bar.bobberBarHeight * Config.FA_RelativeOffsetBarPercentage);
+
+        // Enforce general speed limit
+        if (Math.Abs(bar.bobberBarSpeed) > Config.FA_MaxBarVelocity)
+            return bar.bobberBarSpeed > 0;
+
+        // Enforce a target velocity when the fish is inside the bar
+        if (IsPosInsideBar(bar.bobberBarHeight, bar.bobberBarPos, targetPos))
+        {
+            float targetVelocity = FA_BobberBarTargetVelocity(
+                Math.Abs(barCenter - targetPos),
+                Math.Max(Config.FA_MinBarTargetVelocity, Math.Abs(bar.bobberSpeed)),
+                Config.FA_MaxBarVelocity,
+                0.01F,
+                (bar.bobberBarHeight / 2)
+            );
+
+            if (Math.Abs(bar.bobberBarSpeed) > targetVelocity)
+                return bar.bobberBarSpeed > 0;
+        }
+
+        // Enforce speed limit near bottom
+        if ((bar.bobberBarPos > barMax - Config.FA_BottomThreshold) && bar.bobberBarSpeed > Config.FA_MaxBarVelocityNearBottom)
+            return true;
+
+        // Otherwise, accelerate towards target
+        return targetPos < barCenter;
+    }
+
+    // Parabolic, higher speed allowed as fish reaches outer portion of bar
+    private double FA_BobberBarTargetVelocityFn(double v) => v * v;
+    private float FA_BobberBarTargetVelocity(float currentDelta, float minVel, float maxVel, float minDelta, float maxDelta)
+    {
+        if (currentDelta == 0.0F || minDelta == 0.0F || maxDelta == 0.0F)
+            return minVel; // Deltas were zero, returning minVel
+
+        // Parabolic, higher speed allowed as fish reaches outer portion of bar
+        double oldMinVel = FA_BobberBarTargetVelocityFn((double)minDelta);
+        double oldMaxVel = FA_BobberBarTargetVelocityFn((double)maxDelta);
+
+        // Normalize the delta between the two values.
+        float newVel = (float)((((maxVel - minVel) / (oldMaxVel - oldMinVel)) * (FA_BobberBarTargetVelocityFn((double)currentDelta) - oldMaxVel)) + maxVel);
+        return newVel; // Setting new max velocity
+    }
+
+    private bool IsPosInsideBar(int barSize, float barPos, float pos) =>
+        (pos >= barPos) && (pos <= barPos + barSize);
+}
